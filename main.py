7261dee4f6c2adcf20bec0e1c01e271aef63646d
gui-santoss <guilhermealmeida101906@gmail.com>
2025-11-09 23:56:21 -0300

    final version

--- main.py
+++ main.py
@@ -1,61 +1,58 @@
 #!/usr/bin/env pybricks-micropython
-
-"""
-Example LEGO® MINDSTORMS® EV3 Robot Educator Driving Base Program
------------------------------------------------------------------
-
-This program requires LEGO® EV3 MicroPython v2.0.
-Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
-
-Building instructions can be found at:
-https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
-"""
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, UltrasonicSensor, ColorSensor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
-import random
 
-# Initialize the EV3 Brick.
 ev3 = EV3Brick()
 
-# Initialize the motors.
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
-# Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-obstacle_sensor= UltrasonicSensor(Port.S3)
-
+obstacle_sensor = UltrasonicSensor(Port.S3)
 color_sensor = ColorSensor(Port.S4)
 
+# Estado para alternância de curvas: começa virando à direita.
+last_turn = 'l' 
 
-def turn_to(side):
+def turn_to(side: str):
+    """Faz a manobra de desvio para o lado indicado ('r' ou 'l')."""
     if side == 'r':
         robot.turn(90)
-        robot.straight(400)
+        robot.straight(300)
         robot.turn(-90)
     else:
         robot.turn(-90)
-        robot.straight(400)
+        robot.straight(300)
         robot.turn(90)
-    drive_ahead()
 
 def drive_ahead():
-    sides = ['r', 'l']
-    turn = random.choice(sides)
-    cor = color_sensor.color()
-    while cor != Color.WHITE or color_sensor.reflection() < 60:
-        if cor != Color.WHITE or color_sensor.reflection() < 60:
-            break
+    global last_turn
+
+    while True:
         cor = color_sensor.color()
+        refl = color_sensor.reflection()
+
+        # Condição de parada caso identifique a cor branca.
+        if cor == Color.WHITE and refl >= 60:
+            robot.stop()
+            break
+
+        # Segue em frente.
         robot.drive(125, 0)
+
+        # Verifica se há obstáculo próximo.
         if obstacle_sensor.distance() < 200:
-            if turn == 'r':
-                turn_to('l')
-            else:
-                turn_to('r')
-                      
+            robot.stop()
+            # Se a última foi 'r', agora será 'l', e vice-versa.
+            next_turn = 'l' if last_turn == 'r' else 'r'
+            # Chama a função que faz o carro virar
+            turn_to(next_turn)
+            last_turn = next_turn  
+            # Pausa para estabilizar sensores/mecânica.
+            wait(100)
+
 drive_ahead()
